4a7b004227445d80402e4a9ebfc005fcb20fd700
JasonTsao <lanevo6tmegsr@gmail.com>
2014-12-14 16:53:35 -0800

    adding admin pages and importing settings correctly

---
+++ exercises/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from models import *
+
+admin.site.register(Muscle)
+admin.site.register(Equipment)
+admin.site.register(Excercise)
+admin.site.register(ExcerciseHistory)
+

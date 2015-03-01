55a449f02dea8e728c18001e32e3f600817c59ea
JasonTsao <lanevo6tmegsr@gmail.com>
2015-03-01 11:48:28 -0800

    adding cors middleware

--- MuscleTracker/settings.py
+++ MuscleTracker/settings.py
@@ -103,6 +103,7 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
